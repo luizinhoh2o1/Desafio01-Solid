ed4d1e8616ccde31abd547298222fbbaa0e6b21c
Alessandro Luiz <alessandroluizfacul@gmail.com>
2022-04-13 19:05:59 -0300

    final

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (userAlreadyExists === undefined) {
+      throw new Error("User not exists.");
+    }
+
+    const user = this.usersRepository.turnAdmin(userAlreadyExists);
+    
+    return user;
   }
 }
 
